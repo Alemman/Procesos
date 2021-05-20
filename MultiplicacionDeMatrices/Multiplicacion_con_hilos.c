18ad09817e55e6b6e121f07950c25293f9f14d32
Alemman <gstavo.aleman@gmail.com>
2021-05-20 00:32:14 -0500

    Multiplicacion de hilos

--- MultiplicacionDeMatrices/Multiplicacion_con_hilos.c
+++ MultiplicacionDeMatrices/Multiplicacion_con_hilos.c
@@ -1,28 +1,41 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<pthread.h>
 #include<sys/types.h>
 
 typedef struct{
-    pthread tid;
+    pthread_t tid;
     long TID;
-    char[N] M1;
-    char[N] M2;
+    char* M1;
+    char* M2;
     int row;
     int col;
     int sum;
 }TMultMat;
+int N = 0;
 
-Print_result(long *Matrix_thread, int row, int col);
+
 void *multiply_row_x_Col(void*parameter);
+void Print_result(long** M, int row, int col);
+void * multiply_row_x_Col(void *p);
+void read_row(char *f, int row, int *vector);
+void read_col(char *f, int col, int *vector);
 
 int main(int argc, char *argv[]){
+
+    if(argc != 3){
+	printf("faltan parametros\n");
+	exit(1);
+    }
+
     //checar parametrso completos
-    FILE file = fopen(argv[1], "rb");
+    FILE *file = fopen(argv[1], "rb");
     if(!file){
+	printf("file not found\n");
         exit(-1);
     }
 
-    void * value = NULL;
+    void* value = NULL;
     int i, j, x = 0, y = 0, coutn = 0;
     int row, col;
     long thread_id, number_threads;
@@ -31,9 +44,11 @@ int main(int argc, char *argv[]){
     fread(&col, sizeof(int), 1, file);
     fclose(file);
     
+    N = row;
     long Result[row][col];
     number_threads = row*col;
     TMultMat multMatrix[row][col];
+
 
     for(i = 0; i < row; i++){
         for(j = 0; j < col; j++){
@@ -42,20 +57,21 @@ int main(int argc, char *argv[]){
             multMatrix[i][j].row = i;
             multMatrix[i][j].col = j;
 
-            pthread_create(&multMatrix[i][j].tid, NULL, multiply_row_x_Col, &multMatrix);
+            pthread_create(&multMatrix[i][j].tid, NULL, multiply_row_x_Col, (&multMatrix[i][j]));
             multMatrix[i][j].TID = (unsigned long)multMatrix[i][j].tid;
             Result[i][j] = multMatrix[i][j].TID;
-        }
+	}
     }
 
     pthread_join(multMatrix[x][y].tid, &value);
     thread_id = (long)value;
+
     while(coutn < number_threads){
         for(i = 0; i < row; i++){
             for(j = 0; j < col; j++){
                 if(thread_id == Result[i][j]){
                     Result[i][j] = multMatrix[i][j].sum;
-                    break;
+		    break;
                 }
             }
         }
@@ -69,61 +85,74 @@ int main(int argc, char *argv[]){
         coutn++;
     }
 
-    Print_result(Result);
+   for(int k = 0; k < row; k++){
+        for(int z = 0; z < col; z++){
+            printf("%ld ", Result[k][z]);
+        }
+        printf("\n");
+    }
+
+    
     exit(0);
 }
 
-void Print_result(long M[N][N], int row, int col){
+void Print_result(long** M, int row, int col){
     for(int i = 0; i < row; i++){
         for(int j = 0; j < col; j++){
-            printf("%lld ", M[i][j]);
+            printf("%ld ", M[i][j]);
         }
         printf("\n");
     }
 }
 
 void * multiply_row_x_Col(void *p){
-    TMultMat Matrix_thread = (TMultMat)p;
-    FILE file = fopen(Matrix_thread.M1, "rb");
+    TMultMat *Matrix_thread = (TMultMat*)p;
+    FILE *file = fopen(Matrix_thread->M1, "rb");
     if(!file){
-        printf("Error en apertura de archivo %s\n", Matrix_thread.M1);
+        printf("Error en apertura de archivo hh\n");
         exit(-1);
     }
     int row, col, sum;
     fread(&row, sizeof(int), 1, file);
     fread(&col, sizeof(int), 1, file);
     fclose(file);
-
+	
     int vector_row[row];
     int vector_col[col];
 
-    read_row(Matrix_thread.M1, Matrix_thread.row, vector_row);
-    read_col(Matrix_thread.M2, Matrix_thread.col, vector_col);
+    read_row(Matrix_thread->M1, Matrix_thread->row, vector_row);
+    read_col(Matrix_thread->M2, Matrix_thread->col, vector_col);
 
     for(int i = 0; i < col; i++){
         sum += vector_col[i] * vector_row[i];
+	 
     }
+    
+    Matrix_thread->sum = sum;
 
-    pthread_exit(NULL);
+    pthread_exit((void*)(Matrix_thread->TID));
 
 }
 
-void read_row(char f[], int row, int vector[]){
-    FILE file = fopen(f, "rb");
+void read_row(char *f, int row, int *vector){
+
+    FILE *file = fopen(f, "rb");
     if(!file){
         printf("File Open Error read_row()\n");
         exit(1);
     }
     int n;
     fread(&n, sizeof(int), 1, file);
-    int shifting = sizeof(int)*2 + col*n*(sizeof(int))
+    int shifting = sizeof(int)*2 + row*n*(sizeof(int));
     fseek(file, shifting, SEEK_SET);
     fread(vector, sizeof(int), n, file);
     fclose(file);
+
 }
 
-void read_col(char *f, int col, int vector){
-    FILE file = fopen(f, "rb");
+void read_col(char *f, int col, int *vector){
+
+    FILE *file = fopen(f, "rb");
     if(!file){
         printf("File Open Error read_col() \n");
         exit(1);
@@ -132,10 +161,12 @@ void read_col(char *f, int col, int vector){
     fseek(file, sizeof(int), SEEK_SET);
     fread(&n, sizeof(int), 1, file);
     shifting = sizeof(int)*(n-1);
-
+    fseek(file, sizeof(int) * col, SEEK_CUR);
+    
     for(int i = 0; i < n; i++){
         fread(&vector[i], sizeof(int), 1, file);
         fseek(file, shifting, SEEK_CUR);
+	
     }
 
     fclose(file);
